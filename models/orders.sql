527a79cab38295fee63ce79e9d8b0288715c493d
Carly Kaufman <carly@fishtownanalytics.com>
2020-07-14 20:00:05 +0000

    wip

--- models/orders.sql
+++ models/orders.sql
@@ -1,7 +1,8 @@
 select 
     orders.order_id
     ,orders.customer_id
-    ,payments.amount_usd
+    ,sum(payments.amount_usd) as amount_usd
 from {{ref('stg_orders')}} as orders
 left join {{ref('stg_payments')}} as payments 
     on payments.order_id = orders.order_id
+group by 1, 2
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,1 +1,6 @@
-select * from raw.stripe.payment
+select 
+    id
+    ,orderid as order_id
+    ,status as payment_status
+    ,amount/100 as amount_usd
+from raw.stripe.payment
